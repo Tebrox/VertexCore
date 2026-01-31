c89f93f01d6494d7ad386f6db8875a5a8e9a9698
Tebrox <tebrox90@googlemail.com>
2026-01-31 06:56:18 +0100

    Initial commit

---
+++ src/main/java/de/tebrox/vertexCore/VertexCore.java
@@ -0,0 +1,17 @@
+package de.tebrox.vertexCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VertexCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
